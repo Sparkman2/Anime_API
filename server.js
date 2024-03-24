52cb63cf87372bec16b8b7831540afe17e4f7409
Chirath <cilukpi@u.rochester.edu>
2024-03-24 00:49:29 -0400

    Figured how to use ejs with node!

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const axios = require('axios'); 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`running on port ${PORT}`));
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hi! This is my Anime API Viewer!' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
